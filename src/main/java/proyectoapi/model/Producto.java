bdedb1530a5b2fec53a2ff6ee57f1d82ca06348e
gkrayeski8 <gkrayeski@uade.edu.ar>
2026-04-14 00:35:15 -0300

    db config

--- src/main/java/proyectoapi/model/Producto.java
+++ src/main/java/proyectoapi/model/Producto.java
@@ -1,5 +1,16 @@
 package proyectoapi.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "productos")
 public class Producto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
